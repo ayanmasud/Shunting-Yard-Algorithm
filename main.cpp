3657c2770227ebaba36bdc31641079e5f384a938
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2025-02-26 13:19:24 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,18 +1,65 @@
 #include <iostream>
 #include <cstring>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 struct Node {
   char value;
-  struct Node* next;
+  //struct Node next;
 
   Node() {
     next = NULL;
   }
 };
 
+struct Queue {
+  char* value;
+  struct Queue* next;
+
+  Queue() {
+    next = NULL;
+  }
+};
+void enqueue(Queue* &head, char* &data);
+void dequeue(Queue* head);
+
 int main() {
+  //Stack* stackHead = NULL; // head of stack // I DONT THINK I CAN DO STACKS LIKE THIS. THIS IS NOT A LINKED LIST
+  stack<Node> st;
+  Node na = new Node();
+  char aa = 'a';
+  na.value = 'a';
+  Node nb = new Node();
+  char bb = 'b';
+  nb.value = 'b';
+  st.push(na);
+  st.push(nb);
+
+  while (!st.empty()) {
+    cout << ' ' << st.top();
+    st.pop();
+  }
+
+  Queue* queueHead = NULL; // head of queue
+
+  char a = 'a';
+  char* ap = &a;
+  char b = 'b';
+  char* bp = &b;
+  char c = '+';
+  char* cp = &c;
+  enqueue(queueHead, ap);
+  enqueue(queueHead, bp);
+  enqueue(queueHead, cp);
+
+  // PRINTING INDIV NOT WORKING FOR SOME GOOFY REASON
+  if (queueHead->next != NULL) {//->value;
+    cout << queueHead->next->value;
+  }
+
+  
+  
   char input[20];
   cin.getline(input, 20);
   int inputLen = strlen(input);
@@ -27,7 +74,7 @@ int main() {
   // attempt to do shunting yard algorithm
   for (int i = 0; i < inputLen; i++) {
     if (input[i] == '^') { // ^ operator
-      if (opStack[opStackIndex-1] == '^') { // same precedence
+      /*if (opStack[opStackIndex-1] == '^') { // same precedence
 	int len = strlen(opStack);
 	for (int j = 0; j < len; j++) {
 	  if (opStack[opStackIndex-1] == '(') { // create barrier at (
@@ -40,11 +87,12 @@ int main() {
         }
         opStack[opStackIndex] = input[i];
         opStackIndex++;
-      }
-      else { // since the rest are smaller, just add it to the next
-	opStack[opStackIndex] = '^';
-	opStackIndex++;
-      }
+	}*/
+      //else {
+      // since the rest are smaller or same, just add it to the next
+      opStack[opStackIndex] = '^';
+      opStackIndex++;
+	//}
     }
     else if (input[i] == '*' || input[i] == '/') { // * or / operators
       if (opStack[opStackIndex-1] == '-' || opStack[opStackIndex-1] == '+') { // previous is smaller
@@ -96,17 +144,17 @@ int main() {
       opStackIndex++;
     }
     else if (input[i] == ')') { // parenthesis ended
-      cout << "len" << strlen(opStack) << endl;
+      //cout << "len" << strlen(opStack) << endl;
       int len = strlen(opStack);
       for (int j = 0; j < len; j++) {
-	cout << output << endl;
-	cout << "fwhs" << endl;
-	cout << opStack << endl;
-	cout << opStackIndex << endl;
+	//cout << output << endl;
+	//cout << "fwhs" << endl;
+	//cout << opStack << endl;
+	//cout << opStackIndex << endl;
 	if (opStack[opStackIndex-1] == '(') { // stop at (
 	  opStack[opStackIndex-1] = '\0';
 	  opStackIndex--;
-	  cout << "happened" << endl;
+	  //cout << "happened" << endl;
 	  break;
 	}
 	output[outputIndex] = opStack[opStackIndex-1];
@@ -121,8 +169,8 @@ int main() {
     }
   }
 
-  cout << strlen(opStack) << endl;
-  cout << output << endl;
+  //cout << strlen(opStack) << endl;
+  //cout << output << endl;
   int len = strlen(opStack);
   for (int j = 0; j < len; j++) {
     output[outputIndex] = opStack[opStackIndex-1];
@@ -134,4 +182,56 @@ int main() {
   cout << output;
 
   return 0;
+}
+
+
+
+
+
+
+/*class Queue {
+private:
+  char* value;
+  Queue* next;
+public:
+  // Constructor to initialize the queue
+  Queue() {    
+    next = NULL;
+  }
+  
+  // Destructor to free the allocated memory
+  ~Queue() {
+    delete value;
+    next = NULL;
+  }
+  };*/
+
+void enqueue(Queue* &head, char* &data) {
+  // Add the element to the rear
+  if (head == NULL){
+    Queue* newHead = new Queue();
+    newHead->value = data;
+    head = newHead;
+  }
+  else if (head->next == NULL) {
+    Queue* newNode = new Queue();
+    newNode->value = data;
+    head->next = newNode;
+  }
+  else {
+    enqueue(head->next, data); // recurse until the end of the queue
+  }
+}
+
+void dequeue(Queue* head) {
+  /*if (size == 0) {
+    cout << "Queue is empty" << endl;
+    return;
+  }
+  
+  // Shift all elements to the left by one position
+  for (int i = 1; i < size; i++) {
+    queue[i - 1] = queue[i];
+  }
+  size--;*/
 }
