2c013661a0fc7d80143e86516ac74ec1c87a6ad3
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2025-03-03 22:48:54 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstring>
-//#include <bits/stdc++.h>
+// GOT THE PUSH POP WORKING YAY
+// cleaned up the code as well
+// next class figure out how to implement the peek in the algorithm. maybe replace the opStack[opStackIndex-1] or something. im going to sleep now
 
 using namespace std;
 
@@ -13,8 +15,11 @@ struct Node {
     value = nullptr;
   }
 };
+
 void enqueue(Node* &head, char* &data);
-void dequeue(Node* head);
+char* dequeue(Node* &head);
+void push(Node* &head, char* &data);
+char* pop(Node* &head);
 
 void enqueue(Node* &head, char* &data) {
   Node* newNode = new Node();
@@ -32,176 +37,130 @@ void enqueue(Node* &head, char* &data) {
   }
 }
 
-Node* push(Node* &head, char* &data) {
+char* dequeue(Node* &head) {
+  if (head == NULL) {
+    return nullptr;
+  }
+  Node* temp = head;
+  char* data = temp->value;
+  head = head->next;
+  delete temp;
+  return data;
+}
+
+void push(Node* &head, char* &data) {
   Node* newNode = new Node();
   newNode->value = data;
   newNode->next = head;
-  return newNode;
+  head = newNode;
 }
 
-char* pop(Node* &head) { // CREATE A DESTRUCToR
-  char* data = head->value;
-  Node* temp = head->next;
-  //delete head;
-  head = temp;
+char* pop(Node* &head) {
+  if (head == NULL) {
+    return nullptr;
+  }
+  Node* temp = head;
+  char* data = temp->value;
+  head = head->next;
+  delete temp;
   return data;
 }
-Node* push(Node* &head, char* &data);
-char* pop(Node* &head);
 
 int main() {
-  /*Stack* stackHead = NULL; // head of stack 
-  Stack* na = new Stack();
-  char aa = 'a';
-  na.value = 'a';
-  Node nb = new Node();
-  char bb = 'b';
-  nb.value = 'b';
-  st.push(na);
-  st.push(nb);
-
-  
-  while () {
-    cout << ' ' << st.top();
-    st.pop();
-    }*/
-
   Node* queueHead = NULL; // head of queue
   Node* stackHead = NULL; // head of stack
-
-  
-  
   
   char input[20];
   cin.getline(input, 20);
   int inputLen = strlen(input);
   
-  //char operators[5] = {'+', '-', 'x', '/'};
   char opStack[20];
   int opStackIndex = 0;
 
   char output[20];
   int outputIndex = 0;
   
-  // attempt to do shunting yard algorithm
+  // shunting yard algorithm
   for (int i = 0; i < inputLen; i++) {
     if (input[i] == '^') { // ^ operator
-      /*if (opStack[opStackIndex-1] == '^') { // same precedence
-	int len = strlen(opStack);
-	for (int j = 0; j < len; j++) {
-	  if (opStack[opStackIndex-1] == '(') { // create barrier at (
-	    break;
-          }
-	  output[outputIndex] = opStack[opStackIndex-1];
+      opStack[opStackIndex] = '^';
+      char* pushed = &(input[i]);
+      push(stackHead, pushed);
+      opStackIndex++;
+    }
+    else if (input[i] == '*' || input[i] == '/') { // * or / operators
+      if (opStackIndex == 0 || opStack[opStackIndex-1] == '+' || opStack[opStackIndex-1] == '-') { // previous is smaller or end of stack
+        opStack[opStackIndex] = input[i];
+        char* pushed = &(input[i]);
+        push(stackHead, pushed);
+        opStackIndex++;
+      } else { // previous is greator or same
+        while (opStackIndex > 0 && opStack[opStackIndex-1] != '(' && opStack[opStackIndex-1] != '+' && opStack[opStackIndex-1] != '-') { // create a barrier at ( and also continue with the the + and -
+          output[outputIndex] = opStack[opStackIndex-1];
+          char* popped = pop(stackHead); // pop
+          enqueue(queueHead, popped); // enqueue
           outputIndex++;
-	  opStack[opStackIndex-1] = '\0';
           opStackIndex--;
         }
         opStack[opStackIndex] = input[i];
-        opStackIndex++;
-	}*/
-      //else {
-      // since the rest are smaller or same, just add it to the next
-      opStack[opStackIndex] = '^'; // push
-      opStackIndex++;
-	//}
-    }
-    else if (input[i] == '*' || input[i] == '/') { // * or / operators
-      if (opStack[opStackIndex-1] == '-' || opStack[opStackIndex-1] == '+') { // previous is smaller
-	opStack[opStackIndex] = input[i]; // push
-	char* pushed = &(input[i]);
-        stackHead = push(stackHead, pushed);
-        opStackIndex++;
-      }
-      else { // previous is greater or same
-	int len = strlen(opStack);
-	for (int j = 0; j < len; j++) {
-	  if (opStack[opStackIndex-1] == '(') { // create a barrier at (
-            break;
-          }
-	  if (opStack[opStackIndex-1] == '+' || opStack[opStackIndex-1] == '-') {
-	    break;
-	  }
-          output[outputIndex] = opStack[opStackIndex-1];
-	  char* popped = pop(stackHead); // pop
-	  enqueue(queueHead, popped); // enqueue
-	  outputIndex++;
-	  opStack[opStackIndex-1] = '\0';
-          opStackIndex--;
-        }
-        opStack[opStackIndex] = input[i]; // push
-	char* pushed = &(input[i]);
-	stackHead = push(stackHead, pushed);
+        char* pushed = &(input[i]);
+        push(stackHead, pushed); // push
         opStackIndex++;
       }
     }
     else if (input[i] == '+' || input[i] == '-') { // + or - operators
       if (opStackIndex == 0) { // if its the first in the stack.
-	opStack[opStackIndex] = input[i]; // push
-	char* pushed = &(input[i]);
-        stackHead = push(stackHead, pushed);
-	opStackIndex++;
-      }
-      else { // if not, its guaranteed to be the same or of lower precedence
-	int len = strlen(opStack);
-	for (int j = 0; j < len; j++) {
-	  if (opStack[opStackIndex-1] == '(') { // create barrier at (
-	    break;
-	  }
-	  output[outputIndex] = opStack[opStackIndex-1];
-	  char* popped = pop(stackHead); // pop
+        opStack[opStackIndex] = input[i];
+        char* pushed = &(input[i]);
+        push(stackHead, pushed);
+        opStackIndex++;
+      } else { // if not, its gauranteed to be the same or of lower precedence
+        while (opStackIndex > 0 && opStack[opStackIndex-1] != '(') { // create a barrier at (
+          output[outputIndex] = opStack[opStackIndex-1];
+          char* popped = pop(stackHead); // pop
           enqueue(queueHead, popped); // enqueue
-	  outputIndex++;
-	  opStack[opStackIndex-1] = '\0';
-	  opStackIndex--;
-	}
-	opStack[opStackIndex] = input[i]; // push
-	char* pushed = &(input[i]);
-        stackHead = push(stackHead, pushed);
-	opStackIndex++;
+          outputIndex++;
+          opStackIndex--;
+        }
+        opStack[opStackIndex] = input[i];
+        char* pushed = &(input[i]);
+        push(stackHead, pushed); // push
+        opStackIndex++;
       }
     }
     else if (input[i] == '(') { // inside a parenthesis!
-      opStack[opStackIndex] = input[i]; // push
+      opStack[opStackIndex] = input[i];
       char* pushed = &(input[i]);
-      stackHead = push(stackHead, pushed);
+      push(stackHead, pushed); // push
       opStackIndex++;
     }
     else if (input[i] == ')') { // parenthesis ended
-      //cout << "len" << strlen(opStack) << endl;
-      int len = strlen(opStack);
-      for (int j = 0; j < len; j++) {
-	if (opStack[opStackIndex-1] == '(') { // stop at (
-	  opStack[opStackIndex-1] = '\0';
-	  char* popped = pop(stackHead); // pop
-	  opStackIndex--;
-	  break;
-	}
-	output[outputIndex] = opStack[opStackIndex-1];
-	char* popped = pop(stackHead); // pop
-	enqueue(queueHead, popped); // enqueue
-	outputIndex++;
-	opStack[opStackIndex-1] = '\0';
-	opStackIndex--;
+      while (opStackIndex > 0 && opStack[opStackIndex-1] != '(') { // stops at (
+        output[outputIndex] = opStack[opStackIndex-1];
+        char* popped = pop(stackHead); // pop
+        enqueue(queueHead, popped); // enqueue
+        outputIndex++;
+        opStackIndex--;
+      }
+      if (opStackIndex > 0 && opStack[opStackIndex-1] == '(') { // removes it after the stop
+        char* popped = pop(stackHead); // pop
+        opStackIndex--;
       }
     }
     else { // not an operator
       output[outputIndex] = input[i];
-      char* cp = &(output[outputIndex]); // enqueue
-      enqueue(queueHead, cp);
+      char* cp = &(output[outputIndex]);
+      enqueue(queueHead, cp); // enqueue
       outputIndex++;
     }
   }
 
-  //cout << strlen(opStack) << endl;
-  //cout << output << endl;
-  int len = strlen(opStack);
-  for (int j = 0; j < len; j++) {
+  // print the string version (delete this when done. just use this for comparison)
+  while (opStackIndex > 0) {
     output[outputIndex] = opStack[opStackIndex-1];
-    char* popped = pop(stackHead); // pop
-    enqueue(queueHead, popped); // enqueue
+    char* popped = pop(stackHead);
+    enqueue(queueHead, popped);
     outputIndex++;
-    opStack[opStackIndex-1] = '\0';
     opStackIndex--;
   }
 
@@ -211,7 +170,7 @@ int main() {
   if (queueHead != NULL) {
     Node* temp = queueHead;
     while (temp != NULL) {
-      cout << *temp->value << " ";  // Dereference value to print the character
+      cout << *temp->value << " ";
       temp = temp->next;
     }
     cout << endl;
