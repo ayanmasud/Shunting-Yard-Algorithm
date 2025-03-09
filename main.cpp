098ba6bc51e74710b2987767da67bb04dcfad123
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2025-03-08 22:46:26 -0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <cstring>
-#include "btn.h"
-// got the peek working. that was actually really easy. now time to figure out the binary tree
+// good progress today maybe. took forever but got the postfix and infix to finally work and after cleaning up some code and adding comments, it stopped working bruh. im gonna figure this out tomorrow
 
 using namespace std;
 
-struct Node {
+struct Node { // used for making the linked list for the stacks and queues in the shunting yard algorithm
   char* value;
   struct Node* next;
 
@@ -15,6 +14,64 @@ struct Node {
   }
 };
 
+// moved the btn file here because this is what i wanted to do initially and i dont have to swap tabs so much.
+class btn { // binary tree node class
+public:
+  char* value;
+  btn* left;
+  btn* right;
+
+  // constructor requires setting its value
+  btn(char* val) {
+    value = val;
+    left = nullptr;
+    right = nullptr;
+  }
+
+  // destructor
+  ~btn() {
+    delete[] value;
+  }
+
+  // set left child
+  void setLeft(btn* node) {
+    left = node;
+  }
+
+  // set right child
+  void setRight(btn* node) {
+    right = node;
+  }
+
+  // get left
+  btn* getLeft() {
+    return left;
+  }
+
+  // get the right child
+  btn* getRight() {
+    return right;
+  }
+
+  // get the char value
+  char* getValue() {
+    return value;
+  }
+};
+
+// stack for storing the btns. this is a linked list
+struct btnStack {
+  btn* value;
+  btnStack* next;
+
+  // constructor sets the value which is a btn when called
+  btnStack(btn* node) {
+    next = nullptr;
+    value = node;
+  }
+};
+
+// prototypes
 void enqueue(Node* &head, char* &data);
 char* dequeue(Node* &head);
 void push(Node* &head, char* &data);
@@ -22,15 +79,19 @@ char* pop(Node* &head);
 char* peek(Node* &head);
 void pushTree(btnStack*& head, btn* node);
 btn* popTree(btnStack*& head);
+void printPostfix(btn* node);
+void printInfix(btn* node);
+void printPrefix(btn* node);
 
+// enqueue to the queue
 void enqueue(Node* &head, char* &data) {
-  Node* newNode = new Node();
+  Node* newNode = new Node(); // makes a new node to modify and add
   newNode->value = data;
   newNode->next = NULL;
   
-  if (head == NULL) {
+  if (head == NULL) { // this becomes the head if the queue is empty
     head = newNode;
-  } else {
+  } else { // add the node to the end of the queue linked list (last in)
     Node* temp = head;
     while (temp->next != NULL) {
       temp = temp->next;
@@ -39,10 +100,13 @@ void enqueue(Node* &head, char* &data) {
   }
 }
 
+// dequeue from the queue
 char* dequeue(Node* &head) {
-  if (head == NULL) {
+  if (head == NULL) { // empty
     return nullptr;
   }
+
+  // take the heads value and remove it (first out)
   Node* temp = head;
   char* data = temp->value;
   head = head->next;
@@ -50,14 +114,15 @@ char* dequeue(Node* &head) {
   return data;
 }
 
-void push(Node* &head, char* &data) {
+// push pop peek functions for stacks
+void push(Node* &head, char* &data) { // first in
   Node* newNode = new Node();
   newNode->value = data;
   newNode->next = head;
   head = newNode;
 }
 
-char* pop(Node* &head) {
+char* pop(Node* &head) { // first out
   if (head == NULL) {
     return nullptr;
   }
@@ -68,51 +133,78 @@ char* pop(Node* &head) {
   return data;
 }
 
-char* peek(Node* &head) {
+char* peek(Node* &head) { // peek at the first
   if (head == NULL) {
     return nullptr;
   }
   return head->value;
 }
 
-struct btnStack {
-  btn* value;
-  btnStack* next;
-
-  btnStack(btn* node) {
-    next = NULL;
-    value = node;
-  }
-};
-
 // push and pops, similar to the stack earlier
-void pushTree(btnStack*& head, btn* node) {
-    btnStack* newNode = new btnStack(node);
-    newNode->next = head;
-    head = newNode;
+void pushTree(btnStack*& head, btn* node) { // first in
+  btnStack* newNode = new btnStack(node); // make a new one to make first
+  newNode->next = head;
+  head = newNode;
 }
 
-btn* popTree(btnStack*& head) {
-    if (!head) return nullptr;
-    btnStack* temp = head;
-    btn* node = temp->value;
-    head = head->next;
-    delete temp;
-    return node;
+btn* popTree(btnStack*& head) { // first out
+  if (!head) { 
+    return nullptr;
+  }
+  btnStack* temp = head;
+  btn* node = temp->value;
+  head = head->next;
+  delete temp;
+  return node;
 }
 
-void printTree(btn* root, int depth = 0) {
-    if (!root) return;
-    printTree(root->right, depth + 1);
-    for (int i = 0; i < depth; i++) cout << "  ";
-    cout << *root->value << endl;
-    printTree(root->left, depth + 1);
+// functions for printing the postfix, infix, and prefix from the trees head
+void printPostfix(btn* node) {
+  if (node == nullptr) { // stop
+    return;
+  }
+
+  printPostfix(node->getLeft()); // go through the left half of the tree
+  printPostfix(node->getRight()); // also go through the right half
+  cout << *node->getValue(); // print the value of the node
+}
+
+void printInfix(btn* node) {
+  if (node == nullptr) { // stop
+    return;
+  }
+
+  // put starting parenthesis if its a left or right child
+  if (node->getLeft() != nullptr || node->getRight() != nullptr) {
+      cout << "(";
+  }
+
+  printInfix(node->getLeft()); // go through the left half of the tree
+  cout << *node->getValue(); // print that value, typically an operator
+  printInfix(node->getRight()); // go through the right
+
+  // put ending parenthesis for the same reason after it printed
+  if (node->getLeft() != nullptr || node->getRight() != nullptr) {
+      cout << ")";
+  }
+}
+
+void printPrefix(btn* node) {
+  if (node == nullptr) { // stop
+    return;
+  }
+
+  cout << *node->getValue(); // print the character
+  printPrefix(node->getLeft()); // go through the left of the tree
+  printPrefix(node->getRight()); // then go through the right
 }
 
 int main() {
   Node* queueHead = NULL; // head of queue
   Node* stackHead = NULL; // head of stack
   
+  cout << "Type an expression in infix notation: ";
+
   char input[20];
   cin.getline(input, 20);
   int inputLen = strlen(input);
@@ -127,18 +219,14 @@ int main() {
   for (int i = 0; i < inputLen; i++) {
     if (input[i] == '^') { // ^ operator
       opStack[opStackIndex] = '^';
-      char* pushed = &(input[i]);
-      push(stackHead, pushed);
+      char* pushed = new char(input[i]);
+      push(stackHead, pushed); // push
       opStackIndex++;
     }
     else if (input[i] == '*' || input[i] == '/') { // * or / operators
-      if (opStackIndex == 0 || *(peek(stackHead)) == '+' || *(peek(stackHead)) == '-') {//opStack[opStackIndex-1] == '+' || opStack[opStackIndex-1] == '-') { // previous is smaller or end of stack
-	//char test = opStack[opStackIndex-1];
-	//char* ptest = *test;
-	//char* test = peek(stackHead);
-	//cout << "test" << *test << endl;
-	opStack[opStackIndex] = input[i];
-        char* pushed = &(input[i]);
+      if (opStackIndex == 0 || *(peek(stackHead)) == '+' || *(peek(stackHead)) == '-') { // previous is smaller or end of stack
+	      opStack[opStackIndex] = input[i];
+        char* pushed = new char(input[i]);
         push(stackHead, pushed);
         opStackIndex++;
       } else { // previous is greator or same
@@ -150,7 +238,7 @@ int main() {
           opStackIndex--;
         }
         opStack[opStackIndex] = input[i];
-        char* pushed = &(input[i]);
+        char* pushed = new char(input[i]);
         push(stackHead, pushed); // push
         opStackIndex++;
       }
@@ -158,7 +246,7 @@ int main() {
     else if (input[i] == '+' || input[i] == '-') { // + or - operators
       if (opStackIndex == 0) { // if its the first in the stack.
         opStack[opStackIndex] = input[i];
-        char* pushed = &(input[i]);
+        char* pushed = new char(input[i]);
         push(stackHead, pushed);
         opStackIndex++;
       } else { // if not, its gauranteed to be the same or of lower precedence
@@ -170,14 +258,14 @@ int main() {
           opStackIndex--;
         }
         opStack[opStackIndex] = input[i];
-        char* pushed = &(input[i]);
+        char* pushed = new char(input[i]);
         push(stackHead, pushed); // push
         opStackIndex++;
       }
     }
     else if (input[i] == '(') { // inside a parenthesis!
       opStack[opStackIndex] = input[i];
-      char* pushed = &(input[i]);
+      char* pushed = new char(input[i]);
       push(stackHead, pushed); // push
       opStackIndex++;
     }
@@ -202,18 +290,16 @@ int main() {
     }
   }
 
-  // print the string version (delete this when done. just use this for comparison)
-  while (opStackIndex > 0) {
+  // add the rest of the stack to the queue cause its now done with the other chars
+  /*while (opStackIndex > 0) {
     output[outputIndex] = opStack[opStackIndex-1];
     char* popped = pop(stackHead);
     enqueue(queueHead, popped);
     outputIndex++;
     opStackIndex--;
-  }
+  }*/
 
-  cout << output << endl;
-
-  // display the queue:
+  /*// display the queue:
   if (queueHead != NULL) {
     Node* temp = queueHead;
     while (temp != NULL) {
@@ -221,31 +307,42 @@ int main() {
       temp = temp->next;
     }
     cout << endl;
-  }
+  }*/
 
   // creating the tree
   btnStack* treeStack = nullptr; // binary tree stack head
-  btn* test = NULL;
-  while (queueHead != NULL) {
-    // create and prepare the node
-    char* val = dequeue(queueHead);
-    btn* newNode = new btn();
-    newNode->value = val;
-    
-    if (*val == '+' || *val == '-' || *val == '*' || *val == '/' || *val == '^') { // we need to make the last two in the stack combine under a tree
-      newNode->right = pop(treeStack);
-      newNode->left = pop(treeStack);
-    }
+  while (queueHead != nullptr) {
+      char* val = dequeue(queueHead); // char value from the dequeue
+      btn* newNode = new btn(val); // new parent
 
-    // otherise, push it into the stack
-    push(treeStack, newNode);
+      if (*val == '+' || *val == '-' || *val == '*' || *val == '/' || *val == '^') { // we need to make the last two in the stack combine under a tree
+          if (treeStack != nullptr) {
+              newNode->setRight(popTree(treeStack)); // set the right child
+          }
+          if (treeStack != nullptr) {
+              newNode->setLeft(popTree(treeStack)); // set the left child
+          }
+      }
+
+      // otherise, push it into the stack
+      pushTree(treeStack, newNode);
   }
-  
-  cout << endl;
 
   // now we should only have one thing left in the stack. we'll just put that node into it's own thing
-  btn* treeFinish = pop(treeStack);
-  
+  btn* treeFinish = popTree(treeStack);
+
+  // print the infix postfix and previx
+  cout << endl << "Infix: ";
+  printInfix(treeFinish);
+  cout << endl;
+
+  cout << endl << "Postfix: ";
+  printPostfix(treeFinish);
+  cout << endl;
+
+  cout << endl << "Prefix: ";
+  printPrefix(treeFinish);
+  cout << endl;
   
   return 0;
 }
